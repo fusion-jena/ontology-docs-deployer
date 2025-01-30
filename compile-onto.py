78c46c25e546c6d9f9182dda1ddb87eb5dc70b17
mender <maximilian.enderling@uni-jena.de>
2025-01-30 03:13:49 +0100

    copy to github workspace afterwards

--- compile-onto.py
+++ compile-onto.py
@@ -1,12 +1,10 @@
 from git import Repo
-from shutil import copytree, rmtree
+from shutil import copytree, rmtree, move
 from natsort import natsorted
 import subprocess
-from pathlib import Path
 
 rmtree("./copy", ignore_errors=True)
 copytree(".", "./copy")
-raise Exception(str(Path.cwd().absolute().as_posix()))
 
 repo = Repo.init("./copy")
 tags = natsorted(repo.tags, key= lambda t: t.name)
@@ -14,4 +12,5 @@ tags = natsorted(repo.tags, key= lambda t: t.name)
 for tag in tags:
     repo.git.checkout(tag)
     subprocess.run(f"java -jar widoco.jar -ontFile copy/gerps-datafield.ttl -import copy/gerps-datafield.ttl -outFolder out/{tag.name[1:]} -rewriteAll -getOntologyMetadata -lang de-en -saveConfig out/config -webVowl -noPlaceHolderText", shell=True)
-    
+
+move("/out", "/github/workspace")
