3f37830b763f8e2c0673c6b658727feb179be9bb
mender <maximilian.enderling@uni-jena.de>
2025-01-30 17:48:25 +0100

    put latest ontology doc in root

--- compile-onto.py
+++ compile-onto.py
@@ -14,3 +14,5 @@ tags = natsorted(repo.tags, key= lambda t: t.name)
 for tag in tags:
     repo.git.checkout(tag)
     subprocess.run(f"java -jar /usr/local/widoco/widoco.jar -ontFile copy/gerps-datafield.ttl -import copy/gerps-datafield.ttl -outFolder out/{tag.name[1:]} -rewriteAll -getOntologyMetadata -lang de-en -saveConfig out/config -webVowl -noPlaceHolderText", shell=True)
+    if tag == tags[-1]:
+        copytree(f"out/{tag.name[1:]}", "out/")
