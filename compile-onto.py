53a79793571e0217b593c19bb542f4da563767c1
mender <maximilian.enderling@uni-jena.de>
2025-01-30 18:12:49 +0100

    save htaccess too (even though github pages will not use iit)

--- compile-onto.py
+++ compile-onto.py
@@ -13,6 +13,6 @@ tags = natsorted(repo.tags, key= lambda t: t.name)
 
 for tag in tags:
     repo.git.checkout(tag)
-    subprocess.run(f"java -jar /usr/local/widoco/widoco.jar -ontFile copy/gerps-datafield.ttl -import copy/gerps-datafield.ttl -outFolder out/{tag.name[1:]} -rewriteAll -getOntologyMetadata -lang de-en -saveConfig out/config -webVowl -noPlaceHolderText", shell=True)
+    subprocess.run(f"java -jar /usr/local/widoco/widoco.jar -ontFile copy/gerps-datafield.ttl -import copy/gerps-datafield.ttl -outFolder out/{tag.name[1:]} -rewriteAll -getOntologyMetadata -lang de-en -saveConfig out/config -webVowl -noPlaceHolderText -htaccess", shell=True)
     if tag == tags[-1]:
         copytree(f"out/{tag.name[1:]}", "out/", dirs_exist_ok=True)
