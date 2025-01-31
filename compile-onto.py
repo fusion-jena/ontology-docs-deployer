05e8e2f4dcd3e9fadd9ec24d1ee96324a07cc10a
BMI24 <mitgliedschaftfo@gmx.de>
2025-01-31 23:22:53 +0100

    fix python script trying to access ontology in wrong palce

--- compile-onto.py
+++ compile-onto.py
@@ -20,6 +20,6 @@ tags = natsorted([t for t in repo.tags if t.name.startswith('v')], key= lambda t
 
 for tag in tags:
     repo.git.checkout(tag)
-    subprocess.run(f"java -jar /usr/local/widoco/widoco.jar -ontFile copy/{onto_name}.ttl -import copy/{onto_name}.ttl -outFolder out/{tag.name[1:]} -rewriteAll -getOntologyMetadata -lang de-en -saveConfig out/config -webVowl -noPlaceHolderText -htaccess", shell=True)
+    subprocess.run(f"java -jar /usr/local/widoco/widoco.jar -ontFile copy/ontology/{onto_name}.ttl -import copy/ontology/{onto_name}.ttl -outFolder out/{tag.name[1:]} -rewriteAll -getOntologyMetadata -lang de-en -saveConfig out/config -webVowl -noPlaceHolderText -htaccess", shell=True)
     if tag == tags[-1]:
         copytree(f"out/{tag.name[1:]}", "out/", dirs_exist_ok=True)
