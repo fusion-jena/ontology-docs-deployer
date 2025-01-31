67aa3eb7ea9528c2a8da75991ff9faffd78654c2
BMI24 <mitgliedschaftfo@gmx.de>
2025-01-31 23:18:05 +0100

    make python script compatible with different ontology names

--- compile-onto.py
+++ compile-onto.py
@@ -4,15 +4,22 @@ from natsort import natsorted
 import subprocess
 from pathlib import Path
 from glob import glob
+from os.path import basename
+
+
+onto_files = [basename(f) for f in glob('/github/workspace/ontology/*.ttl')]
+onto_files.sort(key=len)
+onto_name = onto_files[0][:-4]
+
 
 rmtree("./copy", ignore_errors=True)
 copytree("/github/workspace", "./copy")
 
 repo = Repo.init("./copy")
-tags = natsorted(repo.tags, key= lambda t: t.name)
+tags = natsorted([t for t in repo.tags if t.name.startswith('v')], key= lambda t: t.name)
 
 for tag in tags:
     repo.git.checkout(tag)
-    subprocess.run(f"java -jar /usr/local/widoco/widoco.jar -ontFile copy/gerps-datafield.ttl -import copy/gerps-datafield.ttl -outFolder out/{tag.name[1:]} -rewriteAll -getOntologyMetadata -lang de-en -saveConfig out/config -webVowl -noPlaceHolderText -htaccess", shell=True)
+    subprocess.run(f"java -jar /usr/local/widoco/widoco.jar -ontFile copy/{onto_name}.ttl -import copy/{onto_name}.ttl -outFolder out/{tag.name[1:]} -rewriteAll -getOntologyMetadata -lang de-en -saveConfig out/config -webVowl -noPlaceHolderText -htaccess", shell=True)
     if tag == tags[-1]:
         copytree(f"out/{tag.name[1:]}", "out/", dirs_exist_ok=True)
