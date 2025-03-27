fe87e53d330dc948fa6e9ae64c789983ee0ac6a3
BMI24 <maximilian.enderling@uni-jena.de>
2025-03-27 10:31:14 +0100

    maybe fix last evenings commit messup

--- compile-onto.py
+++ compile-onto.py
@@ -7,18 +7,15 @@ from glob import glob
 from os.path import basename
 from os import chdir
 
-def create_docs(onto_basepath : str, out_path : str):
-    subprocess.run(f"java -jar /usr/local/widoco/widoco.jar -ontFile {onto_basepath}.ttl -import {onto_basepath}.ttl -outFolder {out_path} -rewriteAll -getOntologyMetadata -lang de-en -saveConfig out/config -webVowl -noPlaceHolderText -uniteSections", shell=True)
-    diagram_path = f"{onto_basepath}_diagram.svg"
+def create_docs(onto_name : str, out_path : str):
+    subprocess.run(f"java -jar /usr/local/widoco/widoco.jar -ontFile copy/ontology/{onto_name}.ttl -import copy/ontology/{onto_name}.ttl -outFolder {out_path} -rewriteAll -getOntologyMetadata -lang de-en -saveConfig out/config -webVowl -noPlaceHolderText -uniteSections", shell=True)
+    diagram_path = f"copy/ontology/{onto_name}_diagram.svg"
     copyfile("/usr/local/widoco/default_index.html", f"{out_path}/index.html")
     if Path(diagram_path).is_file():
-        copyfile(diagram_path, f"{onto_basepath}_diagram.svg")
+        copyfile(diagram_path, f"out/{onto_name}_diagram.svg")
 
 root = "/github/workspace"
 chdir(root)
-
-
-
 
 rmtree("./copy", ignore_errors=True)
 copytree(root, "./copy")
@@ -28,12 +25,12 @@ tags = natsorted([t for t in repo.tags if t.name.startswith('v')], key= lambda t
 
 for tag in tags:
     repo.git.checkout(tag)
-    out_path = f"out/{tag.name[1:]}"
-    
-    onto_files = [f for f in glob(root + '/copy/ontology/*.ttl')]
+
+    onto_files = [basename(f) for f in glob('copy/ontology/*.ttl')]
     onto_files.sort(key=len)
-    onto_basepath = onto_files[0][:-4]
+    onto_name = onto_files[0][:-4]
     
-    create_docs(onto_basepath, out_path)
+    out_path = f"out/{tag.name[1:]}"
+    create_docs(onto_name, out_path)
     if tag == tags[-1]:
         copytree(out_path, "out/", dirs_exist_ok=True)
